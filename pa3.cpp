721e7ac9740b1bcef4dae45ede9c7e1f7920e6b8
Jiayang Li <gscsdyang@gmail.com>
2017-03-30 20:41:46 +0000

    split input string

--- pa3.cpp
+++ pa3.cpp
@@ -4,31 +4,101 @@
 #include "pa3.h"
 
 using namespace std;
-
+bool hasPlus = false;
+bool hasMinus = false;
+bool hasStar = false;
+bool hasForwardSlash = false;
+bool hasPlusPlus  = false;
+bool hasEquals = false;
+bool hasComma = false;
+bool hasSemicolon = false;
+bool hasFor = false;
+bool hasBegin = false;
+bool hasEnd = false;
 
 int main (){
 	vector <string> filelines;
 	vector <string> parted;
 	
     string filename;
+    string type;
+    
     cout << "Please enter the name of the input file:" <<endl;
     cin >> filename;
     ifstream file;
     file.open(filename);
     if (file.fail()){
-        cout << "file not open!";
+        cout << "ERROR FAIL TO OPEN FILE" <<endl;
         return 1;
     }
     int count = 0;
     while( file.good() ) {
 		std::string token;
 		file >> token;
-		std::cout << "Token " << count++ << ": " << token << std::endl;
+		cout << "Token " << count++ << ": " << token << std::endl;
+		for (unsigned int i = 0; i < token.size(); i++){
+			filelines.push_back(token.substr(i,1));
+		}
+		
+	}
+	for ( unsigned int i = 0; i < filelines.size(); i++){
+		cout << filelines[i] << " " <<endl;
 	}
 	
 	
 	file.close();
 	return 0;
+}
+
+// vector<string> toVector(string s) {
+// 	vector <string> words;
+// 	string word;
+// 	for (int i; i<s.size(); i++){
+		
+// 	}
+	
+// }
+
+bool isOperator(string s){
+	bool check = false;
+	if (s == "+"){
+		check = true;
+		hasPlus = true;
+	}
+	else if(s == "-"){
+		check = true;
+		hasMinus = true;
+	}
+	else if(s == "*"){
+		check = true;
+		hasStar = true;
+	}
+	else if(s == "/"){
+		check = true;
+		hasForwardSlash = true;
+	}
+	else if(s == "++"){
+		check = true;
+		hasPlusPlus = true;
+	}
+	else if(s == "="){
+		check = true;
+		hasEquals = true;
+	}
+	return check;
+}
+
+bool isDelimiter(string s) {
+	bool check = false;
+	if (s == ","){
+		check = true;
+		hasComma = true;
+	}
+	else if (s == ";"){
+		check = true;
+		hasSemicolon = true;
+	}
+	return check;
 }
 
 bool isLowercase(string s) {
@@ -46,5 +116,6 @@ bool isUppercase(string s) {
 	}
 	return true;
 }
+
 
 
--- pa3.h
+++ pa3.h
@@ -4,11 +4,7 @@
 
 using namespace std;
 
-string keyword[];
-string identi[];
-string constant[];
-string operators[];
-string delimiters[];
+
 
 string filename;
 int depth;
