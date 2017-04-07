614832d018b464a721e6fc7881a6539a645a6b5a
Jiayang Li <gscsdyang@gmail.com>
2017-04-07 22:44:29 +0000

    everything should work now, except need to fix -Wall errors

--- pa3.cpp
+++ pa3.cpp
@@ -1,28 +1,84 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 #include "pa3.h"
 
 using namespace std;
-bool hasPlus = false;
-bool hasMinus = false;
-bool hasStar = false;
-bool hasForwardSlash = false;
-bool hasPlusPlus  = false;
-bool hasEquals = false;
-bool hasComma = false;
-bool hasSemicolon = false;
-bool hasFor = false;
-bool hasBegin = false;
-bool hasEnd = false;
+
+void checkIden(string line, vector <string> &iden);
+void checkCons(string line, vector <string> &cons);
+void checkErrors(string line, vector <string> &errors);
+bool checkSyntax (string s);
+void checkEnd(vector <string> &errors, bool end);
+void checkParen(string line, vector <string> &errors, bool &paren);//parenthesis syntax error
+
+void Stack::push(string data){
+	vect.push_back(data);
+}
+
+void Stack::pop(){
+	if (vect.size() > 0){
+		vect.pop_back();
+		currentCount++;
+		if (currentCount > maxCount){
+			maxCount = currentCount;
+		}
+	}
+}
+
+void Stack::clean(){
+	for (int i; i < vect.size(); i ++){
+		vect.pop_back();
+		currentCount--;
+	}
+}
+
+int Stack::getMax(){
+	return maxCount;
+}
+
+int Stack::getCurrent(){
+	return currentCount;
+}
+
+void Stack::setMax(int newMax){
+	maxCount = newMax;
+}
+
+void Stack::setCurrent(int newCurrent){
+	currentCount = newCurrent;
+}
+
+int Stack::size(){
+	return vect.size();
+}
 
 int main (){
-	vector <string> filelines;
-	vector <string> parted;
-	
+	vector <string> iden; // vector store all the identifiers
+	vector <string> cons; // vector store all the constants
+	vector <string> errors; // vector store all the syntax errors
+	vector <int> depth; // vector store all the possible depth
+	bool isnest = true; //bool to see if the loops are nested
+	int nestcount = 0;
+	int absnest = 0;
+	int count = 0;
+	bool paren = true;
+	//bunch checks see if what we are looking for are in the txt file
+	bool hasPlus = false;
+	bool hasMinus = false;
+	bool hasStar = false;
+	bool hasForwardSlash = false;
+	bool hasPlusPlus  = false;
+	bool hasEquals = false;
+	bool hasComma = false;
+	bool hasSemicolon = false;
+	bool hasFor = false;
+	bool hasBegin = false;
+	bool hasEnd = false;
+
     string filename;
-    string type;
-    
+    string line;
     cout << "Please enter the name of the input file:" <<endl;
     cin >> filename;
     ifstream file;
@@ -31,91 +87,214 @@ int main (){
         cout << "ERROR FAIL TO OPEN FILE" <<endl;
         return 1;
     }
-    int count = 0;
-    while( file.good() ) {
-		std::string token;
-		file >> token;
-		cout << "Token " << count++ << ": " << token << std::endl;
-		for (unsigned int i = 0; i < token.size(); i++){
-			filelines.push_back(token.substr(i,1));
+    Stack loop;
+    
+    
+    while( getline(file,line) ) {
+	
+		
+		if (line.find("FOR")!=-1){
+			hasFor= true;
+		}
+		if (line.find("BEGIN")!=-1){
+			hasBegin= true;
+			loop.push("BEGIN");
+			
+			if (isnest && paren){
+				nestcount ++;
+			}
+			else if (!isnest && paren){
+				absnest = nestcount;
+				nestcount = 0;
+				nestcount ++;
+				isnest=true;
+			}
+			
+			if (!paren){
+				paren = true;
+			}
+		}
+		if (line.find("END")!=-1){
+			hasEnd= true;
+			loop.pop();
+			isnest = false;
+		}
+		if (line.find(",")!=-1){
+			hasComma= true;
+		}
+		if (line.find(";")!=-1){
+			hasSemicolon= true;
+		}
+		if (line.find(" + ")!=-1){
+			hasPlus= true;
+		}
+		if (line.find("++")!=-1){
+			hasPlusPlus= true;
+		}
+		if (line.find("-")!=-1){
+			hasMinus= true;
+		}
+		if (line.find("*")!=-1){
+			hasStar= true;
+		}
+		if (line.find("/")!=-1){
+			hasForwardSlash= true;
+		}
+		if (line.find("(")!=-1 || line.find(")")!=-1){
+			checkParen(line, errors, paren);
 		}
 		
-	}
-	for ( unsigned int i = 0; i < filelines.size(); i++){
-		cout << filelines[i] << " " <<endl;
+		checkIden(line, iden);
+		checkCons(line, cons);
+		checkErrors(line, errors);
+    }
+    
+
+    count = loop.getMax();
+    
+    if (loop.size() == 0 && nestcount > absnest){
+    	absnest = nestcount;
+    }
+    
+
+	if (loop.size() > 0 && loop.getMax() != 0){
+		count = loop.getMax() - loop.size();
 	}
 	
+	if (loop.getCurrent() < 0 && loop.getMax() != 0){
+		count = loop.getMax() + loop.getCurrent();
+	}
 	
+	if (absnest > 0 && count > absnest){
+		count = absnest;
+	}
+	
+	cout << "The depth of nested loop(s) is ";
+	cout << count << "!";
+	
+	
+	cout <<"\nKeywords: ";
+	if (hasFor) cout<<"FOR ";
+	if (hasBegin) cout<<"BEGIN ";
+	if (hasEnd) cout<<"END";
+	cout <<"\nIdentifiers: ";
+	for (int i = 0; i < iden.size(); i++){
+	    cout<<iden[i] + " ";
+	}
+	cout <<"\nConstants: ";
+	for (int i = 0; i < cons.size(); i++)
+	    cout<<cons[i] + " ";
+	cout <<"\nOperators: ";
+	if (hasPlusPlus) cout<<"++ ";
+	if (hasEquals) cout<<"= ";
+	if (hasPlus) cout<<"+ ";
+	if (hasMinus) cout<<"- ";
+	if (hasStar) cout<<"* ";
+	if (hasForwardSlash) cout<<"/ ";
+	cout <<"\nDelimiters: ";
+	if (hasSemicolon) cout<<"; ";
+	if (hasComma) cout<<", ";
+	
+	cout<<"\nSyntax Error(s):";
+	checkEnd(errors, hasEnd);
+    for (int i = 0; i < errors.size(); i++){
+	    cout<<errors[i] + " ";
+    }
+	if ( errors.size() < 2){
+		cout << "NA" <<endl;
+	}
 	file.close();
 	return 0;
 }
 
-// vector<string> toVector(string s) {
-// 	vector <string> words;
-// 	string word;
-// 	for (int i; i<s.size(); i++){
-		
-// 	}
+
+void checkIden(string line, vector <string> &iden){
+	string identi = "";
+	for(int i = 0; i < line.size(); i++){
+		if (islower(line[i])!=0){
+			identi += line[i];
+		} 
+		if (islower(line[i])==0){
+			iden.push_back(identi);
+			identi = "";
+		}
+	}
+	sort( iden.begin(), iden.end() );
+	iden.erase( unique( iden.begin(), iden.end() ), iden.end() );
+}
+
+
+void checkCons(string line, vector <string> &cons){
+	string conss = "";
+	for(int i = 0; i < line.size(); i++){
+		if (isdigit(line[i])!=0){
+			conss += line[i];
+		} 
+		if (isdigit(line[i])==0){
+			cons.push_back(conss);
+			conss = "";
+		}
+	}
+	sort( cons.begin(), cons.end() );
+	cons.erase( unique( cons.begin(), cons.end() ), cons.end() );
+}
+
+void checkErrors(string line, vector <string> &errors){
+	string uper = "";
+	bool check = true;
+	for(int i = 0; i < line.size(); i++){
+		if (isupper(line[i])!=0){
+			uper += line[i];
+		} 
+		if (isupper(line[i])==0){
+			if(checkSyntax(uper)){
+				errors.push_back(uper);	
+			}
+			uper = "";
+			check = false;
+		}
+	}
+	if (checkSyntax(uper)){
+		errors.push_back(uper);
+	}
+	sort( errors.begin(), errors.end() );
+	errors.erase( unique( errors.begin(), errors.end() ), errors.end() );
+}
+
+bool checkSyntax (string s){
+	if (s == "FOR" || s == "END" || s == "BEGIN")
+	{
+		return false;
+	}
+	else{
+		return true;
+	}
 	
-// }
-
-bool isOperator(string s){
-	bool check = false;
-	if (s == "+"){
-		check = true;
-		hasPlus = true;
-	}
-	else if(s == "-"){
-		check = true;
-		hasMinus = true;
-	}
-	else if(s == "*"){
-		check = true;
-		hasStar = true;
-	}
-	else if(s == "/"){
-		check = true;
-		hasForwardSlash = true;
-	}
-	else if(s == "++"){
-		check = true;
-		hasPlusPlus = true;
-	}
-	else if(s == "="){
-		check = true;
-		hasEquals = true;
-	}
-	return check;
 }
 
-bool isDelimiter(string s) {
-	bool check = false;
-	if (s == ","){
-		check = true;
-		hasComma = true;
+void checkEnd(vector <string> &errors, bool end){
+	if (!end){
+		errors.push_back("END");
 	}
-	else if (s == ";"){
-		check = true;
-		hasSemicolon = true;
-	}
-	return check;
 }
 
-bool isLowercase(string s) {
-	for (int i = 0; i < (signed int)s.size(); ++i) {
-		if (s[i] < 'a' || s[i] > 'z')
-			return false;
+void checkParen(string line, vector <string> &errors, bool &paren){
+	int braIn = 0;
+	int braEnd = 0;
+	for(int i = 0; i < line.size(); i++){
+		if(line[i] == '('){
+			braIn ++;
+		}
+		else if(line[i] == ')'){
+			braEnd ++;
+		}
 	}
-	return true;
-}
-
-bool isUppercase(string s) {
-	for (int i = 0; i < (signed int)s.size(); ++i) {
-		if (s[i] < 'A' || s[i] > 'Z')
-			return false;
+	if (braEnd < braIn){
+		errors.push_back("(");
+		paren = false;
 	}
-	return true;
+	if (braIn < braEnd){
+		errors.push_back(")");
+		paren = false;
+	}
 }
-
-
-
--- pa3.h
+++ pa3.h
@@ -1,14 +1,23 @@
 #include <iostream>
-#include <stdexcept>
-#include <string>
+#include <vector>
 
 using namespace std;
 
+class Stack{
+    vector <string> vect;
+    int maxCount = 0;
+    int currentCount = 0;
+public:
+    void push(string data);
+    void pop();
+    int getMax();
+    int getCurrent();
+    void setMax(int newMax);
+    void setCurrent(int newCurrent);
+    int size();
+    void clean();
+    
+};
 
 
-string filename;
-int depth;
 
-
-bool isLowercase(string s);
-bool isUppercase(string s);
